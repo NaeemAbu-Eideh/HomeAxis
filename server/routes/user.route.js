76f8144756b62b8d303def0925b6159b146f992c
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2026-03-17 16:41:43 +0200

    updates

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
     app.get("/api/users/all", UserController.findAllUsers);
     app.get("/api/users/find-by-id", UserController.findUserById);
     app.get("/api/users/find-by-email", UserController.findUserByEmail);
-    app.get("/api/users/edit-user", UserController.editUser);
-    app.get("/api/users/add-user", UserController.addUser);
-    app.get("/api/users/delete-user", UserController.deleteUser);
+    app.put("/api/users/edit-user", UserController.editUser);
+    app.post("/api/users/add-user", UserController.addUser);
+    app.delete("/api/users/delete-user", UserController.deleteUser);
 }
