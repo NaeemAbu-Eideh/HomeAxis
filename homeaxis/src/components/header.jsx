530855c28dc9465ab8fa35be17d938dc33b0057a
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2026-03-23 14:07:56 +0200

    -add svg logo

--- homeaxis/src/components/header.jsx
+++ homeaxis/src/components/header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Logo from "./logo.jsx";
 
 function Header(props) {
     return (
         <div className={"flex justify-between mx-auto px-[30px] py-[10px]"}>
-            <h1 className={"text-3xl font-bold"}>HomeAxis</h1>
-
+            <div>
+                <Logo className={"text-red-800"}/>
+                <h1 className={"text-2xl font-bold"}>HomeAxis</h1>
+            </div>
         </div>
     );
 }
